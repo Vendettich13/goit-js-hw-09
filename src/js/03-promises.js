9881535d302d7c624f4b42842f9df199266cf6d7
Victor Litvynchuk <vendettich13@icloud.com>
2022-10-08 23:27:41 +0300

    one of the attempt of hw

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import Notiflix from 'notiflix';
+const refs = {
+  form: document.querySelector('.form'),
+  input: document.querySelectorAll('input'),
+};
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    let result = { position, delay };
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(result);
+      } else {
+        reject(result);
+      }
+    }, delay);
+  });
+}
+
+refs.form.addEventListener('submit', startCreate);
+
+function startCreate(e) {
+  e.preventDefault();
+  let delayValue = Number(refs.input[0].value);
+  let stepValue = Number(refs.input[1].value);
+  let amountValue = Number(refs.input[2].value);
+
+  for (let i = 1; i <= amountValue; i += 1) {
+    let position = i;
+    createPromise(position, delayValue)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delayValue += stepValue;
   }
 }
