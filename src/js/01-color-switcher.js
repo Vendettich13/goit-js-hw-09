e371c0fbc83487243aa0b6a24d229781fb78d79a
Victor Litvynchuk <vendettich13@icloud.com>
2022-10-09 20:25:34 +0300

    reworked tasks

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,25 +3,22 @@ const refs = {
   body: document.querySelector('body'),
 };
 let colorTimerId = null;
-let isActive = false;
 
 const getRandomHexColor = function () {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
 
-const startChangeColor = function () {
-  if (isActive === true) {
-    return;
-  } else isActive = true;
+function startChangeColor() {
+  refs.btn[0].setAttribute('disabled', true);
   colorTimerId = setInterval(() => {
     refs.body.style.backgroundColor = getRandomHexColor();
   }, 1000);
-};
+}
 
-const stopChangeColor = function () {
-  isActive = false;
+function stopChangeColor() {
+  refs.btn[0].removeAttribute('disabled');
   clearInterval(colorTimerId);
-};
+}
 
 refs.btn[0].addEventListener('click', startChangeColor);
 refs.btn[1].addEventListener('click', stopChangeColor);
